1cd8ed4e9c11ad91bf1983539a31c7457ca69a13
ysbc1247 <ystc1247@khu.ac.kr>
2023-05-28 17:41:34 +0900

    [FEAT] repository

---
+++ mvp/src/main/java/com/swm/mvp/repository/YoutubeRepository.java
@@ -0,0 +1,7 @@
+package com.swm.mvp.repository;
+
+import com.swm.mvp.entity.Youtube;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface YoutubeRepository extends JpaRepository<Youtube, Long> {
+}
