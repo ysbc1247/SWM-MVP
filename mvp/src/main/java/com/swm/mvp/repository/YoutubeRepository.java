db66ce725989c24c088feb3b5720e47fab2d7d4c
ysbc1247 <ystc1247@khu.ac.kr>
2023-05-30 12:45:23 +0900

    [FEAT] added function for finding all youtube as List by user Id

--- mvp/src/main/java/com/swm/mvp/repository/YoutubeRepository.java
+++ mvp/src/main/java/com/swm/mvp/repository/YoutubeRepository.java
@@ -2,6 +2,8 @@ package com.swm.mvp.repository;
 
 import com.swm.mvp.entity.Youtube;
 import org.springframework.data.jpa.repository.JpaRepository;
+import reactor.core.publisher.Flux;
 
 public interface YoutubeRepository extends JpaRepository<Youtube, Long> {
+    Flux<Youtube> findAllByUserId(Long userId);
 }
