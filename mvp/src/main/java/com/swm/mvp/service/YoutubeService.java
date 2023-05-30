47ef75760397fe7d09d20a448a7ea089c7ab2faf
ysbc1247 <ystc1247@khu.ac.kr>
2023-05-30 09:20:41 +0900

    [FEAT] basic CRUD operations for youtube

--- mvp/src/main/java/com/swm/mvp/service/YoutubeService.java
+++ mvp/src/main/java/com/swm/mvp/service/YoutubeService.java
@@ -1,8 +1,37 @@
 package com.swm.mvp.service;
 
+import com.swm.mvp.entity.Youtube;
+import com.swm.mvp.repository.YoutubeRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class YoutubeService {
-    
+    private final YoutubeRepository youtubeRepository;
+
+    public YoutubeService(YoutubeRepository youtubeRepository) {
+        this.youtubeRepository = youtubeRepository;
+    }
+
+    public Youtube saveYoutube(Youtube youtube) {
+        return youtubeRepository.save(youtube);
+    }
+
+    public Optional<Youtube> findYoutubeById(Long id) {
+        return youtubeRepository.findById(id);
+    }
+
+
+    public void deleteYoutube(Long id) {
+        youtubeRepository.deleteById(id);
+    }
+
+    public List<Youtube> findAllYoutubes() {
+        return youtubeRepository.findAll();
+    }
 }
