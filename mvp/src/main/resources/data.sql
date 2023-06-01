f7cc0a6fa7c65a045e57474d9e6334c8b5527453
ysbc1247 <ystc1247@khu.ac.kr>
2023-06-01 19:05:07 +0900

    [REFACTOR] changed sql statement to suit new user attributes

--- mvp/src/main/resources/data.sql
+++ mvp/src/main/resources/data.sql
@@ -1,1 +1,3 @@
-INSERT INTO users (id, username, password, email) VALUES (1, 'a', 'a', 'dummy@user.com');
+insert into users (user_id, user_password, role_types, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
+                                                                                                                                            ('ystc', '{noop}ystc', 'ADMIN', 'ystc', 'ystc@mail.com', 'I am ystc.', now(), 'ystc', now(), 'ystc')
+;
