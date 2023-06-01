8593a66b9d9121df54d058a9d56f6b2db2637849
ysbc1247 <ystc1247@khu.ac.kr>
2023-06-01 19:07:53 +0900

    [FEAT] converter class for role class flexible use

---
+++ mvp/src/main/java/com/swm/mvp/entity/converter/RoleTypesConverter.java
@@ -0,0 +1,26 @@
+package com.swm.mvp.entity.converter;
+
+import com.swm.mvp.entity.RoleType;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Converter
+public class RoleTypesConverter implements AttributeConverter<Set<RoleType>, String> {
+
+    private static final String DELIMITER = ",";
+
+    @Override
+    public String convertToDatabaseColumn(Set<RoleType> attribute) {
+        return attribute.stream().map(RoleType::name).sorted().collect(Collectors.joining(DELIMITER));
+    }
+
+    @Override
+    public Set<RoleType> convertToEntityAttribute(String dbData) {
+        return Arrays.stream(dbData.split(DELIMITER)).map(RoleType::valueOf).collect(Collectors.toSet());
+    }
+
+}
