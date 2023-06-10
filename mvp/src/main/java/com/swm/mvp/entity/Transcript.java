c739cd7a4d7179864b9351e9ead9912fcbd2a792
ysbc1247 <ystc1247@khu.ac.kr>
2023-06-10 14:29:12 +0900

    [REFACTOR] made transcript a entity instead of element
    - transcript not initialized when fetching in youtube

--- mvp/src/main/java/com/swm/mvp/entity/Transcript.java
+++ mvp/src/main/java/com/swm/mvp/entity/Transcript.java
@@ -1,21 +1,28 @@
 package com.swm.mvp.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Embeddable;
-import javax.persistence.Entity;
-import javax.persistence.Lob;
+import javax.persistence.*;
 
-@Embeddable
+@Entity
 @Getter
 @Setter
 public class Transcript {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String text;
     private Double start;
     private Double duration;
     @Lob
     @Column(length = 16777215)
     private byte[] audio;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "youtube_id", nullable = false)
+    @JsonBackReference
+    private Youtube youtube;
 }
